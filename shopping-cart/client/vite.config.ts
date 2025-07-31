a9a2bf0bfbd0fad32ae17d802db1ff3b70e90b03
任超 <renchao@fanxiapp.com>
2025-07-31 10:41:21 +0800

    feat: 【商城购物车】 打包构建

--- shopping-cart/client/vite.config.ts
+++ shopping-cart/client/vite.config.ts
@@ -1,15 +1,20 @@
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // 加载对应模式的环境变量
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
